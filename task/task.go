427e73993aa8796971def7d6503ced5ef76ce1b9
Wesley Hansen <wesrhansen@gmail.com>
2025-02-03 20:29:05 -0500

    Chapter2 skeleton complete

---
+++ task/task.go
@@ -0,0 +1,39 @@
+package task
+
+import (
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+
+	"time"
+)
+
+type State int
+
+const (
+	Pending = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPOrts  nat.PortSet
+	PortBindings  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
+}
